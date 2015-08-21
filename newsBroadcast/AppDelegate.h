8c45cd257462240c1c76b5a5982e27cdfa5c3637
john-heaven <johnheaven@tom.com>
2015-08-21 10:34:32 +0800

    Initial Commit

---
+++ newsBroadcast/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  newsBroadcast
+//
+//  Created by Mac_jiang on 15/8/21.
+//  Copyright (c) 2015年 Mac_jiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
