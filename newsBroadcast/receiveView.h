523a2030292c95bdf7ab40f772cf92435e8382ca
john-heaven <johnheaven@tom.com>
2015-08-21 11:20:44 +0800

    增加了消息接收界面

---
+++ newsBroadcast/receiveView.h
@@ -0,0 +1,13 @@
+//
+//  receiveView.h
+//  newsBroadcast
+//
+//  Created by Mac_jiang on 15/8/21.
+//  Copyright (c) 2015年 Mac_jiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface receiveView : UIViewController
+
+@end
---
+++ newsBroadcast/receiveView.m
@@ -0,0 +1,13 @@
+//
+//  receiveView.m
+//  newsBroadcast
+//
+//  Created by Mac_jiang on 15/8/21.
+//  Copyright (c) 2015年 Mac_jiang. All rights reserved.
+//
+
+#import "receiveView.h"
+
+@implementation receiveView
+
+@end
